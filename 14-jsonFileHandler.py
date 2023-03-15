dc750206c322cdcce5d91b53e76bbf2ae6f78aba
EdgarVRP <edgarvrp22@gmail.com>
2023-03-14 19:51:33 -0600

    Ejercicios 11-13

---
+++ 14-jsonFileHandler.py
@@ -0,0 +1,10 @@
+import json
+import os
+# def readJsonFile(fileName):
+#     data = ""
+#     try:
+#         with open(fileName) as json_file:
+#             data = json.load(json_file)
+#     except IOError:
+#         print("Could not read file")
+#     return data
